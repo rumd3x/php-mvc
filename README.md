86d198826b981747d6a4ceaf1c569c6624b2b185
edmurcardoso <edmur.cardoso@gmail.com>
2018-03-02 00:19:02 -0300

    Cache Fix

--- README.md
+++
@@ -1,2 +0,0 @@
-# php-mvc
-Simple PHP Base for Apps
--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	ActiveRecord::addEnv('development', array(
-		'driver' => 'mysqli', 
-		'host' => 'mysql.server.com', 
-		'database' => 'testdb', 
-		'username' => 'root', 
-		'password' => ''
-	));
