5727aa25cd47dc17c95aff8a8a1b4661a6c7f8b8
rumd3x <edmurcardoso@gmail.com>
2018-06-06 00:18:48 -0300

    + Static verification functionality

--- system/BaseModel.php
+++ system/BaseModel.php
@@ -6,5 +6,10 @@
 	{
 		protected $dao;		
 	    public abstract function __construct();
+
+	    public static function _isStatic() {
+	        $backtrace = debug_backtrace();
+	        return $backtrace[1]['type'] == '::';
+	    }
 	}
 ?>
